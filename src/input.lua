bed6fe2f9199ca6c72bd736c251016d32ef0b1be
masoniis <masonmbott@gmail.com>
2024-05-30 14:02:52 -0600

    recursive json, added turtle heartbeat

--- src/input.lua
+++ src/input.lua
@@ -1,36 +1,50 @@
 -- Turn a flat table into a JSON string
 local function jsonify(flattable)
-	local jsonString = "{"
-	for k, v in pairs(flattable) do
-		jsonString = jsonString .. "\"" .. k .. "\":" .. "\"" .. v .. "\"" .. ","
+	local function tableToJson(tbl)
+		local jsonString = "{"
+		for k, v in pairs(tbl) do
+			jsonString = jsonString .. "\"" .. k .. "\":"
+			if type(v) == "table" then
+				jsonString = jsonString .. tableToJson(v) .. ","
+			else
+				jsonString = jsonString .. "\"" .. tostring(v) .. "\"" .. ","
+			end
+		end
+		jsonString = jsonString:sub(1, -2) .. "}"
+		return jsonString
 	end
-	jsonString = jsonString:sub(1, jsonString:len() - 1) .. "}"
-	return jsonString
+
+	return tableToJson(flattable)
 end
 
 local function luaify(jsonString)
 	local t = {}
-	print("input:" .. jsonString)
 	jsonString = jsonString:sub(2, jsonString:len() - 1)
-	for pair in string.gmatch(jsonString, "%b\"\":%b\"\"") do
-		print("Pair: " .. pair)
+	for key, value in string.gmatch(jsonString, "(%b\"\"):(%b\"\")") do
+		key = key:sub(2, key:len() - 1)
+		value = value:sub(2, value:len() - 1)
+		t[key] = value
 	end
-	-- for str in string.gmatch(jsonString, "([^" .. "," .. "]+)") do
-	-- 		local key, value = str:match("([^=]+)=(.*)")
-	-- 		print("key: " .. key .. " value: " .. value)
-	--        table.insert(t, { [key] = value })
-	--  end
-
-	print("Stage 2: ")
-	for k, v in pairs(t) do
-		print(k, v)
-	end
-	-- local table = {}
-	-- for k, v in pairs(jsonString) do
-	-- 	table[k] = v
-	-- end
-	print("END")
 	return t
+end
+
+local function turtleStats()
+	local fuelLevel = turtle.getFuelLevel()
+	local blockedForward, info = turtle.inspect()
+	local inventory = {}
+	for i = 1, 16 do
+		inventory[i] = turtle.getItemDetail(i)
+		if inventory[i] == nil then
+			inventory[i] = "empty"
+		end
+	end
+	local stats = {
+		fuelLevel = tostring(fuelLevel),
+		inspect = tostring(blockedForward),
+		inspectInfo = tostring(info),
+		inventory = inventory,
+	}
+	return stats
 end
 
 local wsURL = "ws://73.229.134.181:80"
@@ -41,29 +55,55 @@ local function main()
 	local turtleid = ""
 
 	if ws then
+		print("Websocket success!")
 		ws.send("turtle") -- Send the client tag to the server
 
-		print("WebSocket connection established.")
-		local message = { action = "message", client = "turtle", data = "Hello, world! I am the turtleaa!" }
+		while turtleid == "" do
+			local response, err = ws.receive()
+			if response then
+				local responseT = luaify(response)
+				if responseT.id then
+					turtleid = responseT.id
+				end
+			else
+				print("Failed to receive initial ID: " .. err)
+				break
+			end
+		end
 
-		local jsonString = "{\"action\":\"" .. message.client .. "\",\"data\":\"" .. message.data .. "\"}"
-		print("Jsoned: " .. jsonify(message))
-		print("Sending " .. jsonString)
-
+		print("Turtle ID: " .. turtleid)
+		local message = { action = "newTurtle", id = turtleid, client = "turtle", data = "Hello world, I am a turtle!" }
+		-- print("Sending message: " .. jsonify(message))
 		ws.send(jsonify(message))
 
 		while true do
 			local response, err = ws.receive()
 			if response then
-				print("Received from server: " .. response)
-				print("Luaified: " .. tostring(luaify(response)))
-				local luacode = load(response)
-				if luacode then
-					luacode()
+				local responeTable = luaify(response)
+				if responeTable.action == "requestTurtleStats" then
+					local stats = turtleStats()
+
+					ws.send(jsonify({
+						action = "turtleStats",
+						id = turtleid,
+						client = "turtle",
+						fuelLevel = stats.fuelLevel,
+						inspect = stats.inspect,
+						inspectInfo = stats.inspectInfo,
+						inventory = stats.inventory,
+					}))
+				else
+					local luacode = load(responeTable.data)
+					if luacode then
+						print("Executing code: " .. responeTable.data)
+						luacode()
+					else
+						print("Received non-code message: " .. responeTable.data)
+					end
 				end
 			else
 				print("Failed to receive message: " .. err)
-				break -- Break the loop if there's an error
+				break
 			end
 		end
 
